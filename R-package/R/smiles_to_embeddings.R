26291ad15424c6c73975feafbd6086dc5bec44d8
the-ahuja-lab <60312173+the-ahuja-lab@users.noreply.github.com>
2025-11-19 09:20:21 +0530

    Update smiles_to_embeddings.R

--- R-package/R/smiles_to_embeddings.R
+++ R-package/R/smiles_to_embeddings.R
@@ -7,6 +7,12 @@ library(jsonlite)
 library(data.table)
 library(reticulate)
 
+# --- Configuration (must match the server) ---
+BATCH_SIZE <- 32
+NUM_FEATURES <- 8192 # Assuming this is your feature size
+# In R, we specify the size in bytes. float32 is 4 bytes.
+FLOAT_SIZE_BYTES <- 4
+DTYPE_R <- "numeric" # The 'what' argument for readBin
 
 
 # Before you run the function, you might need to tell reticulate which
@@ -62,13 +68,7 @@ is_valid_smiles <- function(smiles_string) {
 
 # --- Main Function ---
 collect_features_from_csv <- function(filepath, key , convert_to_canonical = TRUE) {
-  # --- Configuration (must match the server) ---
-  BATCH_SIZE <- 32
-  NUM_FEATURES <- 8192 # Assuming this is your feature size
-  # In R, we specify the size in bytes. float32 is 4 bytes.
-  FLOAT_SIZE_BYTES <- 4
-  DTYPE_R <- "numeric" # The 'what' argument for readBin
-
+  
   # --- 1. Pre-process and Validate the Input CSV ---
   library(data.table)
   message("Reading and validating CSV...")
@@ -224,3 +224,5 @@ collect_features_from_csv <- function(filepath, key , convert_to_canonical = TRU
     }
     return(df_features)  # data frame with NUM_ROWS rows and NUM_FEATURES columns
 }
+
+
